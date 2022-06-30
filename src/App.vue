de083faa64d5feca816d6f1688fa1fca076795df
Sanders4Coding <tianjie_1028@163.com>
2022-06-30 11:53:10 +0800

    页面样式基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
-  <nav>
-    <Header/>
-  </nav>
-  <router-view/>
+    <nav>
+        <!--      头部-->
+        <Header/>
+        <!--      主体-->
+        <div style="display: flex">
+            <!--          侧边栏-->
+            <Aside/>
+            <!--          内容区域-->
+            <router-view style="flex: 1;"/>
+        </div>
+    </nav>
 </template>
 
 <style>
@@ -11,11 +18,13 @@
 
 <script>
     import Header from "@/components/Header";
+    import Aside from "@/components/Aside";
 
     export default {
-        name:"Layout",
-        components:{
-            Header
+        name: "Layout",
+        components: {
+            Header,
+            Aside
         }
     }
 </script>
